2e8027af0e9d57a144b5b6a542f1734a4cdc3ce0
GGCrew <R_Connelie@hotmail.com>
2013-10-14 00:52:23 +0000

    Mystery changes!  Committing to prevent loss

--- src/SDK2_simplicity_plus.c
+++ src/SDK2_simplicity_plus.c
@@ -151,8 +151,8 @@ void battery_state_callback(BatteryChargeState battery_charge_state) {
 		else {bitmap_layer_set_bitmap(battery_icon_bitmap_layer, battery_charging_full_image);}
 		layer_set_hidden(bitmap_layer_get_layer(battery_icon_bitmap_layer), false);
 	} else {
-		if(battery_charge_state.charge_percent <= 10) {bitmap_layer_set_bitmap(battery_icon_bitmap_layer, battery_empty_image);}
-		else if(battery_charge_state.charge_percent <= 20) {bitmap_layer_set_bitmap(battery_icon_bitmap_layer, battery_low_image);}
+		if(battery_charge_state.charge_percent <= 5) {bitmap_layer_set_bitmap(battery_icon_bitmap_layer, battery_empty_image);}
+		else if(battery_charge_state.charge_percent <= 10) {bitmap_layer_set_bitmap(battery_icon_bitmap_layer, battery_low_image);}
 		else {bitmap_layer_set_bitmap(battery_icon_bitmap_layer, battery_charging_full_image);}
 		
 		// Hide the battery icon if charge is over 20% (and we're not charging the battery)
